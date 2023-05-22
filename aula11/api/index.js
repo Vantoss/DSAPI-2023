c594c30917caaf0695c21d3619123648f572ae16
Vantoss <80919590+Vantoss@users.noreply.github.com>
2023-05-21 21:49:57 -0300

    Updated aula11 Api (still unfinished)
    gotta finish soon

--- aula11/api/index.js
+++ aula11/api/index.js
@@ -1,5 +1,5 @@
 const restify = require("restify");
-const errors = require ("restify-errors");
+const errors = require("restify-errors");
 
 const server = restify.createServer({
     name : 'loja_dsapi',
@@ -11,7 +11,7 @@ server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
 server.listen(8001, ()=>{
-    console.log("Servidor executando em http://localhost:8001")
+    console.log("%Servidor executando em", server.url)
 });
 
 var knex = require("knex")({
@@ -28,7 +28,70 @@ server.get('/', (req, res, next)=>{
     res.send('Bem vindo(a) á API Loja_DSAPI')
 });
 
-server.put('/cadCidade', (req,res,next)=>{
-
+server.post('/cidades', (req,res,next)=>{
+    knex('cidades').insert(req.body).then((dados) =>{
+        res.send(dados)
+    }, next);
 });
 
+server.get('/cidades', (req, res, next)=>{
+    knex('cidades').then((dados)=>{
+        res.send(dados)
+    }, next);
+});
+
+server.post('/categorias', (req,res,next)=>{
+    knex('categorias').insert(req.body).then((dados) =>{
+        res.send(dados)
+    }, next);
+});
+
+server.get('/categorias', (req, res, next)=>{
+    knex('categorias').then((dados)=>{
+        res.send(dados)
+    }, next);
+});
+
+server.post('/clientes', (req, res, next)=>{
+    knex('clientes').insert(req.body).then((dados) =>{
+        res.send(dados)
+    }, next);
+})
+
+server.get('/clientes', (req, res, next)=>{
+    knex('clientes').then((dados) =>{
+        res.send(dados)
+    }, next);
+})
+
+server.post('/produtos', (req, res, next)=>{
+    knex('produtos').insert(req.body).then((dados) =>{
+        res.send(dados)
+    }, next);
+})
+
+server.get('/produtos', (req, res, next)=>{
+    knex('produtos').then((dados) =>{
+        res.send(dados)
+    }, next);
+});
+
+server.del('/produtos/:idProd', (req, res, next)=>{
+    const idProduto = req.params.idProd;
+    knex('produtos').where('id', idProduto).delete().then((dados) =>{
+        if(!dados){
+            return res.send( new errors.BadRequestError('Produto não encontrado'))
+        };
+        res.send('Produto Deletado')
+    }, next);
+});
+
+server.put('/produtos/:idProd', (req, res, next)=>{
+    const idProduto = req.params.idProd;
+    knex('produtos').where('id', idProduto).update(req.body).then((dados) =>{
+        if(!dados){
+            return res.send( new errors.BadRequestError('Produto não encontrado'))
+        };
+        res.send('Produto Atualizado')
+    }, next);
+});
