8a1e6edc149bf9090dd745301ed42bda9eecf64d
Vantoss <joaoaliomar@hotmail.com>
2023-06-06 11:41:11 -0300

    Update aula12

--- aula12/api/index.js
+++ aula12/api/index.js
@@ -1,5 +1,9 @@
 const restify = require("restify");
-const errors = require("restify-errors");;
+const errors = require("restify-errors");
+const corsMiddleware = require("restify-cors-middleware2");
+const cors = corsMiddleware({
+    origins : ['*']
+});
 
 const servidor = restify.createServer({
     name : 'loja',
@@ -9,6 +13,8 @@ const servidor = restify.createServer({
 servidor.use(restify.plugins.acceptParser(servidor.acceptable));
 servidor.use(restify.plugins.queryParser());
 servidor.use(restify.plugins.bodyParser());
+servidor.pre(cors.preflight);
+servidor.use(cors.actual);
 
 servidor.listen(8001, function(){
     console.log("%s executando em %s", servidor.name, servidor.url)
