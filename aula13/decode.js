d101e42aaf714b8c3dbf25875d717dd96b1ef2b8
Vantoss <joaoaliomar@hotmail.com>
2023-06-27 12:07:03 -0300

    Update oAuth (still unfinished)

---
+++ aula13/decode.js
@@ -0,0 +1,16 @@
+const jose = require('jose');
+
+function decodeJwtResponse(token){
+    jose.decodeJwt(token);
+};
+function handleCredentialResponse(response) {
+    const responsePayload = decodeJwtResponse(response.credential);
+
+    console.log("ID: " + responsePayload.sub);
+    console.log('Full Name: ' + responsePayload.name);
+    console.log('Given Name: ' + responsePayload.given_name);
+    console.log('Family Name: ' + responsePayload.family_name);
+    console.log("Image URL: " + responsePayload.picture);
+    console.log("Email: " + responsePayload.email);
+};
+handleCredentialResponse()
